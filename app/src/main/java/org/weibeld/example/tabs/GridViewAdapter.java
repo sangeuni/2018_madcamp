70d9b8e9e906aabe7ac65ef32b8d16a594a2b4c0
33577 <ukj0806@kaist.ac.kr>
2018-12-28 20:44:22 +0900

    grid view frame

---
+++ app/src/main/java/org/weibeld/example/tabs/GridViewAdapter.java
@@ -0,0 +1,57 @@
+package org.weibeld.example.tabs;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.graphics.Bitmap;
+import org.weibeld.example.R;
+import android.widget.ArrayAdapter;
+import android.app.Activity;
+
+import java.util.ArrayList;
+
+class GridViewAdapter extends BaseAdapter   {
+    private Context context;
+    private int layoutResourceId;
+    private ArrayList<Integer> data = new ArrayList<Integer>();
+    int layout;
+    LayoutInflater inf;
+
+    public GridViewAdapter(Context context, int layout, ArrayList<Integer> data) {
+        this.context = context;
+        this.data = data;
+        this.layout = layout;
+        this.inf = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+    @Override
+    public int getCount() { // 총 데이터의 개수를 리턴
+        return data.size();
+    }
+    @Override
+    public Object getItem(int position) { // 해당번째의 데이터 값
+        return data.get(position);
+    }
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if (convertView == null)
+            convertView = inf.inflate(layout, null);
+
+        ImageView iv = (ImageView)convertView.findViewById(R.id.image);
+        Object m = data.get(position);
+        iv.setImageResource((Integer) m);
+        return convertView;
+
+    }
+
+}
+
--- app/src/main/java/org/weibeld/example/tabs/Page2Fragment.java
+++ app/src/main/java/org/weibeld/example/tabs/Page2Fragment.java
@@ -1,20 +1,70 @@
 package org.weibeld.example.tabs;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import org.weibeld.example.R;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.database.MergeCursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.SimpleAdapter;
+
+import com.bumptech.glide.Glide;
+
+import java.io.File;
+import static android.os.Environment.DIRECTORY_PICTURES;
+
 /* Fragment used as page 2 */
 public class Page2Fragment extends Fragment {
+    GridView galleryGridView;
+    ArrayList<Integer> f = new ArrayList<Integer>();
 
-    @Override
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_page2, container, false);
-        return rootView;
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_page2, container, false);
+        GridView gridView = (GridView) view.findViewById(R.id.gridView);
+
+        f.add(R.drawable.b1);
+        f.add(R.drawable.b2);
+       
+
+        GridViewAdapter adapter = new GridViewAdapter(this.getActivity(), R.layout.album_row, f);
+        gridView.setAdapter(adapter);
+
+        return view;
     }
+
+
+
 
 }
