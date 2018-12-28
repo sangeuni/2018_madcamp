2b56abc47c7d06abda6c5100156984b61243aef2
sangeuni <goakgoak1995@gmail.com>
2018-12-28 21:33:08 +0900

    make Page1Fragment

---
+++ app/src/main/java/org/weibeld/example/tabs/NameListItem.java
@@ -0,0 +1,12 @@
+package org.weibeld.example.tabs;
+
+public class NameListItem {
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+    public NameListItem(String name){
+        this.name = name;
+    }
+}
