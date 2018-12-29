015ea589709c98cf712b8a47065dbd5d4eddef16
33577 <ukj0806@kaist.ac.kr>
2018-12-29 17:02:51 +0900

    tag clicked work

--- app/src/main/java/org/weibeld/example/tabs/Page2Fragment.java
+++ app/src/main/java/org/weibeld/example/tabs/Page2Fragment.java
@@ -1,6 +1,7 @@
 package org.weibeld.example.tabs;
 
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
@@ -44,29 +45,64 @@ import java.io.File;
 import static android.os.Environment.DIRECTORY_PICTURES;
 
 /* Fragment used as page 2 */
-public class Page2Fragment extends Fragment {
+public class Page2Fragment extends Fragment implements View.OnClickListener {
     GridView galleryGridView;
     ArrayList<TagsImg> f = new ArrayList<TagsImg>();
-
+    ArrayList<Integer> activeTagIds = new ArrayList<Integer>();
+    int test=0;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_page2, container, false);
         GridView gridView = (GridView) view.findViewById(R.id.gridView);
 
-        getData();
+        Toast.makeText(getActivity(), "test is "+String.valueOf(activeTagIds), Toast.LENGTH_SHORT).show();
+
+
+        coloringActiveTags();
+        getData(); //set f after f.clear();
+
         GridViewAdapter adapter = new GridViewAdapter(this.getActivity(), R.layout.album_row, f);
         gridView.setAdapter(adapter);
 
-
+        TextView one = (TextView) view.findViewById(R.id.textView0);
+        one.setOnClickListener(this); // calling onClick() method
+        TextView two = (TextView) view.findViewById(R.id.textView1);
+        two.setOnClickListener(this); // calling onClick() method
+        TextView three = (TextView) view.findViewById(R.id.textView2);
+        three.setOnClickListener(this); // calling onClick() method
+        TextView four = (TextView) view.findViewById(R.id.textView3);
+        four.setOnClickListener(this); // calling onClick() method
+        TextView five = (TextView) view.findViewById(R.id.textView4);
+        five.setOnClickListener(this); // calling onClick() method
 
         return view;
     }
+    public void coloringActiveTags() {
+
+    }
+    public void refresh() {
+        Fragment frg = null;
+        frg = this;
+        final FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.detach(frg);
+        ft.attach(frg);
+        ft.commit();
+    }
     public void onClick(View v) {
-        Toast.makeText(getActivity(), "clicked", Toast.LENGTH_SHORT).show();
-            //error
+        // v.getId()를 activeTagIds 넣거나 빼기
+        if ( activeTagIds.contains(v.getId()) ) {
+            activeTagIds.remove(Integer.valueOf(v.getId())); // 그냥 써버리면 index꺼가 사라짐 ㅜㅜ
+        }
+        else activeTagIds.add(v.getId());
+
+        refresh();
     }
     public void getData() {
+        // f 비우기
+        f.clear();
+        // TODO activeTagIds 에 따라서 data 넣기
+
         // 친구, 상속자들, 여행, 특별한 순간
         f.add(new TagsImg(R.drawable.a1, new Integer[]{0, 2}));
         f.add(new TagsImg(R.drawable.a2, new Integer[]{0 }));
@@ -92,4 +128,5 @@ public class Page2Fragment extends Fragment {
         f.add(new TagsImg(R.drawable.a21, new Integer[]{ }));
          */
     }
+
 }
